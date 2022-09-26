c1bce35018564b2d19540ead6f5ca8262fde0fde
rashed <x.suwaidi@gmail.com>
2022-09-26 16:30:13 +0400

    added create view

--- tasker/tasks/urls.py
+++ tasker/tasks/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 from tasker.tasks.views import (
     task_list_view,
-    task_detail_view
+    task_detail_view,
+    task_create_view
 )
 
 app_name = "tasks"
@@ -11,4 +12,5 @@ app_name = "tasks"
 urlpatterns = [
     path("", view=task_list_view, name="list"),
     path("<int:id>/", view=task_detail_view, name="detail"),
+    path("~create/", view=task_create_view, name="create"),
 ]
--- tasker/tasks/views.py
+++ tasker/tasks/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Task
 
 
-class IndexView(ListView):
+class TaskListView(ListView):
     model = Task
 
 
-task_list_view = IndexView.as_view()
+task_list_view = TaskListView.as_view()
 
 
-class ListDetailView(DetailView):
+class TaskDetailView(DetailView):
     model = Task
     slug_field = "id"
     slug_url_kwarg = "id"
 
 
-task_detail_view = ListDetailView.as_view()
+task_detail_view = TaskDetailView.as_view()
 
+
+class TaskCreateView(CreateView, LoginRequiredMixin):
+    model = Task
+    fields = '__all__'
+
+    def get_success_url(self):
+        assert (
+            self.request.user.is_authenticated
+        )  # for mypy to know that the user is authenticated
+        return reverse('tasks:list')
+
+
+task_create_view = TaskCreateView.as_view()
