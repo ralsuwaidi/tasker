18a2e0219f301081f8d0b97bf14b23bd414a25b9
rashed <x.suwaidi@gmail.com>
2022-11-14 16:58:37 +0400

    add alowwed host

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="ot5KhKUHLtWe8yJ0UeT7OatCf8j2dQ7BNfpsDv759uNszAYOvsUh6pqwCRYbHC6L",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "20.203.8.74"]
 
 # CACHES
 # ------------------------------------------------------------------------------
